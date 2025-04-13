83bb489ca4658f3dd12304c3d919e89e77fd07cd
solamdev <lscheffer740@gmail.com>
2025-04-13 15:08:49 -0300

    mudado de http pra https

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cidade = document.getElementById('cidade')
  async function buscar_cep() {
    const valor_cep = cep.value; 
    console.log(valor_cep)
-   await fetch(`http://viacep.com.br/ws/${valor_cep}/json/`)
+   await fetch(`https://viacep.com.br/ws/${valor_cep}/json/`)
        .then( Response => Response.json())
        .then( conteudo => update(conteudo))
  }
