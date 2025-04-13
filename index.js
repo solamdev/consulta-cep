f14c3954fbd0c150208be3faf6a9637f6eb19c98
solamdev <lscheffer740@gmail.com>
2025-04-13 15:01:38 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//teste
+const cep = document.getElementById('cep')
+const rua = document.getElementById('rua')
+const bairro = document.getElementById('bairro')
+const cidade = document.getElementById('cidade')
+
+ async function buscar_cep() {
+   const valor_cep = cep.value; 
+   console.log(valor_cep)
+   await fetch(`http://viacep.com.br/ws/${valor_cep}/json/`)
+       .then( Response => Response.json())
+       .then( conteudo => update(conteudo))
+ }
+
+function limpar_form () {
+    rua.value=("")
+    bairro.value=("")
+    cidade.value=("")
+}
+
+function update (conteudo) {
+    if (!("erro" in conteudo)) {
+        rua.value=(conteudo.logradouro)
+        bairro.value=(conteudo.bairro)
+        cidade.value=(conteudo.localidade)
+    }
+    }
+    
+
